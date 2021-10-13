494087393cf78f135b4416b6201b3d4d7f4fc526
csdoge <bowenlu.cloud@gmail.com>
2021-10-14 00:17:19 +0800

    source code

---
+++ src/ch02/InputTest.java
@@ -0,0 +1,17 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        String name = in.nextLine();
+
+        System.out.println("Please enter your age: ");
+        int age = in.nextInt();
+
+        System.out.println("Hello, " + name +". You will be " + (age + 1) + " years old next year!");
+    }
+}
